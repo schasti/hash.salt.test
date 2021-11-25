0ca8c0c83040a5cd3afbf2e03c15c12e39425ae8
Schasti <imamshah19@yahoo.com>
2021-11-25 14:44:17 +0100

    sidste ændring

--- src/main/java/test1.java
+++ src/main/java/test1.java
@@ -1,28 +1,51 @@
+
 import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureClassLoader;
 import java.security.SecureRandom;
+import java.util.Scanner;
 
 public class test1 {
+
+    static int salt = getSalt();
+
     public static void main(String[] args) {
 
+        int saltgemt = 42069;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("skriv kode");
 
-        String algo = "MD5";
-        String pass="hejmeddig";
-       String hash= generateHash(pass);
-       String hash2 = generateHash("hejmeddig");
-       byte[] salt = getSalt();
-        System.out.println(salt[4]);
-        System.out.println(hash);
-        System.out.println(hash2);
+        String pass= scan.nextLine();
+        //byte[] salt = getSalt();
+       String hash= generateHash(pass,salt);
+       //String hash2 = generateHash("hejmeddig",salt);
 
+        System.out.println("Skriv den samme kode");
+        String pass2 = scan.nextLine();
+        String hash2 = generateHash(pass2,saltgemt);
+        if(hash2==hash){
+            System.out.println("koden matcher");
+        }
+        else{
+            System.out.println("FYFYFY");
+        }
+        System.out.println("hash:   "+hash);
+        System.out.println("salt:   "+salt);
+        System.out.println("hash2:   "+hash2);
+        System.out.println("saltgemt:   "+saltgemt);
+
+
+
+       // System.out.println(hash2);
 
     }
-    public static String generateHash(String pass){
+
+    public static String generateHash(String pass, int salt){
         try {
             MessageDigest md5 = MessageDigest.getInstance("MD5");
+            pass+=salt;
             md5.update(StandardCharsets.UTF_8.encode(pass));
             return String.format("%032x", new BigInteger(1, md5.digest()));
         } catch (NoSuchAlgorithmException e) {
@@ -30,10 +53,14 @@ public class test1 {
         }
         return pass;
     }
-    public static byte[] getSalt(){
+
+    public static int getSalt(){
         byte[] salt = new byte[20];
         SecureRandom sr = new SecureRandom();
         sr.nextBytes(salt);
-        return salt;
+        int saltint=salt[4];
+        return saltint;
     }
+
+
 }
